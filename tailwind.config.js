a71b15190831fc7ac1a24c36c3df1774f2547a07
Tom James <thomjjames@gmail.com>
2022-08-31 10:47:39 +0200

    Initial commit of TWS Tailwind theme

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'templates/**/*.twig',
+    'tw_tws.theme'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
